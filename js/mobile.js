90f426a1091b86e5c41c7c42862f835adfb3e53e
Denis <zhuravlev_denis01@mail.ru>
2021-09-15 19:35:48 +0300

    I made a slider that consists of 4 sliders

--- js/mobile.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.btn-burger').addEventListener('click', (event) => {
-    document.querySelector('.navigation').classList.toggle('navigation--active-nav')
-})
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+  navigation: {
+    prevEl: ".swiper-button-next",
+    nextEl: ".swiper-button-prev",
+  },
+});
